9761427d2ac897ebe40050f92d6de57b09761154
cccy <jul27.cy@gmail.com>
2025-01-21 17:58:27 +0800

    更改表名重新生成代码

--- src/main/java/com/cccy/essayeval/mapper/TUserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.cccy.essayeval.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.cccy.essayeval.entity.TUser;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TUserMapper extends BaseMapper<TUser> {
-}
